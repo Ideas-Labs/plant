1f117c22d6aa473af54810b6ad15b17e261e6314
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-01-15 20:56:30 +0530

    Update setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'plant',
+  packages = ['plant'],
+  version = '0.1',
+  license='MIT',
+  description = 'Plant simplifies Python package management for your projects.',
+  author = 'Ideas Labs',
+  author_email = 'saurabh.chaturvedi63@gmail.com',
+  url = 'https://github.com/Ideas-Labs/plant',
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['pip', 'packaging', 'python'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
